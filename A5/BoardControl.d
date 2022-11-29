ba94aa3f0bb0d53b678ec4ee4443be347c708350
junlin0902 <118623321+junlin0902@users.noreply.github.com>
2022-11-29 05:35:28 -0500

    Delete A5 directory

--- A5/BoardControl.d
+++
@@ -1,2 +0,0 @@
-BoardControl.o: BoardControl.cc BoardControl.h Board.h Block.h Score.h \
- Level.h View.h window.h
--- A5/main.d
+++
@@ -1,2 +0,0 @@
-main.o: main.cc Block.h Level.h Score.h Board.h View.h window.h \
- BoardControl.h
